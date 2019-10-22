ef90b1de3fa6403c7b0f463efaa75e0c551ab569
Tbeaumont79 <tbeaumont79@icloud.com>
2019-10-22 16:55:48 +0200

    i know how to use va_arg method

--- test1.c
+++ test1.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 13:00:10 by thbeaumo          #+#    #+#             */
-/*   Updated: 2019/10/22 13:57:48 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2019/10/22 16:54:58 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,19 +18,23 @@ char *test_1(int i, ...)
 {
 	va_list argptr;
 	va_start(argptr, i);
-	char *str = "\0";
-	while (i)
+	char *str;
+	int size;
+
+	size = 0;
+	if (!(str = (char *)malloc(sizeof(char ) * (i + 1))))
+		return (0);
+	while (size < i)
 	{
-		str[i] = va_arg(argptr, int);
-		i--;
+		str[size] = va_arg(argptr, int);
+		size++;
 	}
 	va_end(argptr);
 	return (str);
 }
 int main(int ac, char **av)
 {
-	(void)ac;
-	char *s = test_1(3, av[1][0], av[2][0], av[3][0]);
+	char *s = test_1(ac - 1, av[1][0], av[2][0], av[3][0]);
 	printf("%s\n",s);
 	return (0);
 }
