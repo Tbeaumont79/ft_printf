90155653d0360358a617d3b606f5e07efe12413c
Tbeaumont79 <tbeaumont79@icloud.com>
2019-11-07 16:01:43 +0100

    still working on \n

--- pf_s.c
+++ pf_s.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 14:02:15 by thbeaumo          #+#    #+#             */
-/*   Updated: 2019/11/07 15:19:59 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2019/11/07 16:01:26 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,10 @@ int     pf_s(va_list ap, t_struct *datas)
 	val = va_arg(ap, char *);
 	if (!(tmp = ft_strdup(val)))
 		return (-1);
-	//printf("%s <- AVANT\n", datas->str);
+	printf("%s <- AVANT\n", datas->str);
 	if (!(datas->str = ft_strjoin(datas->str, tmp)))
 		return (-1);
-	//printf("%s <- APRES\n", datas->str);
+	printf("%s <- APRES\n", datas->str);
     if (!(ft_buffer(datas)))
         return (-1);
     return (1);
