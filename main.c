468983b16f21b026f6c231b25c4581f0da3695ca
Tbeaumont79 <tbeaumont79@icloud.com>
2020-01-02 17:20:25 +0100

    adding header to all file

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 12:08:27 by thbeaumo          #+#    #+#             */
-/*   Updated: 2019/10/23 14:06:34 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2020/01/02 17:02:25 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@
 int main()
 {
    printf("Characters: %c %c \n", 'a', 65);
-   printf("Decimals: %d %ld\n", 1977, 650000L);
-   printf("Preceding with blanks: %10d \n", 1977);
-   printf("Preceding with zeros: %010d \n", 1977);
-   printf("Some different radices: %d %x %o %#x %#o \n", 100, 100, 100, 100, 100);
+   printf("Some different radices: %d %x %#x %#X \n", 100, 100, 100, 100);
    printf("Width trick: %*d \n", 5, 10);
-   printf("%s \n", "A string");
+   printf("string :: %.5s   \n", "coucou");
    return 0;
 }
