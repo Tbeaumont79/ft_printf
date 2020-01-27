d08b19216e2b958599b8fb99d85b4ae1a00893cf
Tbeaumont79 <tbeaumont79@icloud.com>
2020-01-27 11:29:23 +0100

    working on the pf_s

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 12:08:27 by thbeaumo          #+#    #+#             */
-/*   Updated: 2020/01/23 10:38:55 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2020/01/27 08:13:02 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,131 +49,135 @@ static void d()
 	ft_printf("-8.5d %-8.5d\n",34);
 	printf("-5.10d %-5.10d\n",35);
 	ft_printf("-5.10d %-5.10d\n", 35);
+	printf("*.5d %*.5d \n", -5, 12);
+	ft_printf("*.5d %*.5d \n", -5, 12);
 	/*
-	printf("-----------------BASIC---Test-------------------------\n");
-	printf("FOR d :: ---------------------------\n");
-	printf("return :: %d\n", printf("bonjour -7d %-7d \n", -14));
-	printf("return : %d\n",ft_printf("bonjour -7d :: %-7d\n", -14));
-	printf("return :: %d\n", printf("test1 -4d %-4d \n", -94827));
-	printf("return : %d\n",ft_printf("test 1 -4d :: %-4de\n", -94827));
-	printf("return :: %d\n", printf("bonjour %d \n", 12));
-	printf("return : %d\n",ft_printf("basic 12 :: %d\n", 12));
-	printf("return :: %d\n", printf("basic 12 :: %d\n", 12));
-	printf("return :: %d\n", printf("basic 0 :: %d\n", 0));
-	printf("return : %d\n",ft_printf("basic 7 0 :: %7d\n", 0));
-	printf("return :: %d\n", printf("%7d\n", 12));
-	printf("return :: %d\n", printf("-1.5 : %-1.5dbonjour\n", 12));
-	printf("return :: %d\n", printf("01.5 : %01.5dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("01.5 : %01.5dbonjour\n", 12));
-	printf("return :: %d\n", printf("-5.1 : %-5.1dbonjour\n", 12));
-	printf("return :: %d\n", printf("05.2 : %05.2dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("05.2 : %05.2dbonjour\n", 12));
-	printf("return :: %d\n", printf("-10.5 : %-10.5dbonjour\n", 12));
-	printf("return :: %d\n", printf("010.5 : %010.5dbonjour\n", 12));
-	printf("return :: %d\n", printf("10d : %10dbonjour\n", 12));
-	printf("return :: %d\n", printf("0*.*d : %0*.*d\n", -5, 10, 12));
-	printf("return :: %d\n", printf("05d with neg : %05d\n", -12));
-	printf("return :: %d\n", printf("05d with neg : %05.8d\n", -12));
-	printf("return : %d\n", printf("05.8d with neg : %05.8d\n", -12));
-	printf("return :: %d\n", printf("-5d with neg : %-5d\n", -12));
-	printf("return :: %d\n", printf("05d with neg : %05d\n", 12));
-	printf("\n ---------MY-PRINTF---------- \n");
-	ft_printf("-----------------BASIC---Test-------------------------\n");
-	ft_printf("FOR d :: ---------------------------\n");
-	printf("return : %d\n", ft_printf("05d with neg : %05d\n", -12));
-	printf("return : %d\n", ft_printf("-5d with neg : %-5d\n", -12));
-	printf("return : %d\n", ft_printf("-5.8d with neg : %-5.8d\n", -12));
-	printf("return : %d\n", ft_printf("05d with neg : %05d\n", 12));
-	printf("return : %d\n",ft_printf("-1.5 : %-1.5dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("-5.1 : %-5.1dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("-10.5 : %-10.5dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("010.5 : %010.5dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("10d : %10dbonjour\n", 12));
-	printf("return : %d\n",ft_printf("0*.*d : %0*.*d\n", -5, 10, 12));
-	*/
+	   printf("-----------------BASIC---Test-------------------------\n");
+	   printf("FOR d :: ---------------------------\n");
+	   printf("return :: %d\n", printf("bonjour -7d %-7d \n", -14));
+	   printf("return : %d\n",ft_printf("bonjour -7d :: %-7d\n", -14));
+	   printf("return :: %d\n", printf("test1 -4d %-4d \n", -94827));
+	   printf("return : %d\n",ft_printf("test 1 -4d :: %-4de\n", -94827));
+	   printf("return :: %d\n", printf("bonjour %d \n", 12));
+	   printf("return : %d\n",ft_printf("basic 12 :: %d\n", 12));
+	   printf("return :: %d\n", printf("basic 12 :: %d\n", 12));
+	   printf("return :: %d\n", printf("basic 0 :: %d\n", 0));
+	   printf("return : %d\n",ft_printf("basic 7 0 :: %7d\n", 0));
+	   printf("return :: %d\n", printf("%7d\n", 12));
+	   printf("return :: %d\n", printf("-1.5 : %-1.5dbonjour\n", 12));
+	   printf("return :: %d\n", printf("01.5 : %01.5dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("01.5 : %01.5dbonjour\n", 12));
+	   printf("return :: %d\n", printf("-5.1 : %-5.1dbonjour\n", 12));
+	   printf("return :: %d\n", printf("05.2 : %05.2dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("05.2 : %05.2dbonjour\n", 12));
+	   printf("return :: %d\n", printf("-10.5 : %-10.5dbonjour\n", 12));
+	   printf("return :: %d\n", printf("010.5 : %010.5dbonjour\n", 12));
+	   printf("return :: %d\n", printf("10d : %10dbonjour\n", 12));
+	   printf("return :: %d\n", printf("0*.*d : %0*.*d\n", -5, 10, 12));
+	   printf("return :: %d\n", printf("05d with neg : %05d\n", -12));
+	   printf("return :: %d\n", printf("05d with neg : %05.8d\n", -12));
+	   printf("return : %d\n", printf("05.8d with neg : %05.8d\n", -12));
+	   printf("return :: %d\n", printf("-5d with neg : %-5d\n", -12));
+	   printf("return :: %d\n", printf("05d with neg : %05d\n", 12));
+	   printf("\n ---------MY-PRINTF---------- \n");
+	   ft_printf("-----------------BASIC---Test-------------------------\n");
+	   ft_printf("FOR d :: ---------------------------\n");
+	   printf("return : %d\n", ft_printf("05d with neg : %05d\n", -12));
+	   printf("return : %d\n", ft_printf("-5d with neg : %-5d\n", -12));
+	   printf("return : %d\n", ft_printf("-5.8d with neg : %-5.8d\n", -12));
+	   printf("return : %d\n", ft_printf("05d with neg : %05d\n", 12));
+	   printf("return : %d\n",ft_printf("-1.5 : %-1.5dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("-5.1 : %-5.1dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("-10.5 : %-10.5dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("010.5 : %010.5dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("10d : %10dbonjour\n", 12));
+	   printf("return : %d\n",ft_printf("0*.*d : %0*.*d\n", -5, 10, 12));
+	   */
 }
-/*
-   static void test_all_flag()
-   {
-   printf("FOR x :: ---------------------------\n");
-   printf("bonjour %x \n", 12);
-   printf("0*.*x : %0*.*xbonjour\n", 10, 5, 12);
-   printf("-*.*x : %-*.*xbonjour\n", 10, 5, 12);
-   printf("010.5x : %010.5xbonjour\n", 12);
-   printf("-10.5x : %-10.5xbonjour\n", 12);
-   printf("0*x : %0*xbonjour\n", 10, 12);
-   printf("-*x : %-*xbonjour\n", 10, 12);
-   printf("FOR X :: ---------------------------\n");
-   printf("bonjour %X \n", 12);
-   printf("0*.*X : %0*.*Xbonjour\n", 10, 5, 12);
-   printf("-*.*X : %-*.*Xbonjour\n", 10, 5, 12);
-   printf("010.5X : %010.5Xbonjour\n", 12);
-   printf("-10.5X : %-10.5Xbonjour\n", 12);
-   printf("0*X : %0*Xbonjour\n", 10, 12);
-   printf("-*X : %-*Xbonjour\n", 10, 12);
-   printf("FOR u :: ---------------------------\n");
-   printf("bonjour 12 %u \n", 12);
-   printf("bonjour -12 %u \n", -12);
-   printf("0*.*u : %0*.*ubonjour\n", 10, 5, 12);
-   printf("-*.*u : %-*.*ubonjour\n", 10, 5, 12);
-   printf("010.5u : %010.5ubonjour\n", 12);
-   printf("-10.5u : %-10.5ubonjour\n", 12);
-   printf("0*u : %0*ubonjour\n", 10, 12);
-   printf("-*u : %-*ubonjour\n", 10, 12);
-   printf("FOR s :: ---------------------------\n");
-   printf("simple test : %s \n", "bonjour");
-   printf(".5s : %.5s \n", "bonjour");
-   printf(".*s : %.*s \n", 5, "bonjour");
-   printf("-10.5s : %-5.5s \n", "bonjour");
-   printf("-*.*s : %-10.5s \n", "bonjour");
-   printf("-*s : %-*s \n", 5, "bonjour");
-   printf("5s : %12s \n", "bonjour");
-   printf("*s : %*s \n", 12, "bonjour");
-   printf("double arg : %s%s\n", "bonjour", "paul");
-   printf("return : %d\n",ft_printf("FOR x :: ---------------------------\n"));
-   printf("return : %d\n",ft_printf("bonjour %x \n", 12));
-   printf("return : %d\n",ft_printf("0*.*x : %0*.*xbonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("-*.*x : %-*.*xbonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("010.5x : %010.5xbonjour\n", 12));
-   printf("return : %d\n",ft_printf("-10.5x : %-10.5xbonjour\n", 12));
-   printf("return : %d\n",ft_printf("0*x : %0*xbonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("-*x : %-*xbonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("FOR X :: ---------------------------\n"));
-   printf("return : %d\n",ft_printf("bonjour %X \n", 12));
-   printf("return : %d\n",ft_printf("0*.*X : %0*.*Xbonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("-*.*X : %-*.*Xbonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("010.5X : %010.5Xbonjour\n", 12));
-   printf("return : %d\n",ft_printf("-10.5X : %-10.5Xbonjour\n", 12));
-   printf("return : %d\n",ft_printf("0*X : %0*Xbonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("-*X : %-*Xbonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("FOR u :: ---------------------------\n"));
-   printf("return : %d\n",ft_printf("bonjour 12 %u \n", 12));
-   printf("return : %d\n",ft_printf("bonjour -12 %u \n", -12));
-   printf("return : %d\n",ft_printf("0*.*u : %0*.*ubonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("-*.*u : %-*.*ubonjour\n", 10, 5, 12));
-   printf("return : %d\n",ft_printf("010.5u : %010.5ubonjour\n", 12));
-   printf("return : %d\n",ft_printf("-10.5u : %-10.5ubonjour\n", 12));
-   printf("return : %d\n",ft_printf("0*u : %0*ubonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("-*u : %-*ubonjour\n", 10, 12));
-   printf("return : %d\n",ft_printf("FOR s  :: ---------------------------\n"));
-   printf("return : %d\n",ft_printf("simple test : %s \n", "bonjour"));
-   printf("return : %d\n",ft_printf(".5s : %.5s \n", "bonjour"));
-   printf("return : %d\n",ft_printf(".*s : %.*s \n", 5, "bonjour"));
-   printf("return : %d\n",ft_printf("-10.5s : %-5.5s \n", "bonjour"));
-   printf("return : %d\n",ft_printf("-*.*s : %-10.5s \n", "bonjour"));
-   printf("return : %d\n",ft_printf("-*s : %-*s \n", 5, "bonjour"));
-   printf("return : %d\n",ft_printf("5s : %12s \n", "bonjour"));
-printf("return : %d\n",ft_printf("*s : %*s \n", 12, "bonjour"));
-printf("return : %d\n",ft_printf("double arg : %s%s\n", "bonjour", "paul"));
-ft_printf("-*.*s : %-*.*s \n", 90, 5, "bonjour");
+static void test_all_flag()
+{
+	printf("FOR x :: ---------------------------\n");
+	printf("bonjour %x \n", 12);
+	printf("0*.*x : %0*.*xbonjour\n", 10, 5, 12);
+	printf("-*.*x : %-*.*xbonjour\n", 10, 5, 12);
+	printf("010.5x : %010.5xbonjour\n", 12);
+	printf("-10.5x : %-10.5xbonjour\n", 12);
+	printf("0*x : %0*xbonjour\n", 10, 12);
+	printf("-*x : %-*xbonjour\n", 10, 12);
+	printf("FOR X :: ---------------------------\n");
+	printf("bonjour %X \n", 12);
+	printf("0*.*X : %0*.*Xbonjour\n", 10, 5, 12);
+	printf("-*.*X : %-*.*Xbonjour\n", 10, 5, 12);
+	printf("010.5X : %010.5Xbonjour\n", 12);
+	printf("-10.5X : %-10.5Xbonjour\n", 12);
+	printf("0*X : %0*Xbonjour\n", 10, 12);
+	printf("-*X : %-*Xbonjour\n", 10, 12);
+	printf("FOR u :: ---------------------------\n");
+	printf("bonjour 12 %u \n", 12);
+	printf("bonjour -12 %u \n", -12);
+	printf("0*.*u : %0*.*ubonjour\n", 10, 5, 12);
+	printf("-*.*u : %-*.*ubonjour\n", 10, 5, 12);
+	printf("010.5u : %010.5ubonjour\n", 12);
+	printf("-10.5u : %-10.5ubonjour\n", 12);
+	printf("0*u : %0*ubonjour\n", 10, 12);
+	printf("-*u : %-*ubonjour\n", 10, 12);
+	printf("FOR s :: ---------------------------\n");
+	printf("simple test : %s \n", "bonjour");
+	printf(".5s : %.5s \n", "bonjour");
+	printf(".*s : %.*s \n", 5, "bonjour");
+	printf("-10.5s : %-5.5s \n", "bonjour");
+	printf("-*.*s : %-10.5s \n", "bonjour");
+	printf("-*s : %-*s \n", 5, "bonjour");
+	printf("5s : %12s \n", "bonjour");
+	printf("*s : %*s \n", 12, "bonjour");
+	printf("double arg : %s%s\n", "bonjour", "paul");
+	printf("return : %d\n",ft_printf("FOR x :: ---------------------------\n"));
+	printf("return : %d\n",ft_printf("bonjour %x \n", 12));
+	printf("return : %d\n",ft_printf("0*.*x : %0*.*xbonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("-*.*x : %-*.*xbonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("010.5x : %010.5xbonjour\n", 12));
+	printf("return : %d\n",ft_printf("-10.5x : %-10.5xbonjour\n", 12));
+	printf("return : %d\n",ft_printf("0*x : %0*xbonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("-*x : %-*xbonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("FOR X :: ---------------------------\n"));
+	printf("return : %d\n",ft_printf("bonjour %X \n", 12));
+	printf("return : %d\n",ft_printf("0*.*X : %0*.*Xbonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("-*.*X : %-*.*Xbonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("010.5X : %010.5Xbonjour\n", 12));
+	printf("return : %d\n",ft_printf("-10.5X : %-10.5Xbonjour\n", 12));
+	printf("return : %d\n",ft_printf("0*X : %0*Xbonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("-*X : %-*Xbonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("FOR u :: ---------------------------\n"));
+	printf("return : %d\n",ft_printf("bonjour 12 %u \n", 12));
+	printf("return : %d\n",ft_printf("bonjour -12 %u \n", -12));
+	printf("return : %d\n",ft_printf("0*.*u : %0*.*ubonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("-*.*u : %-*.*ubonjour\n", 10, 5, 12));
+	printf("return : %d\n",ft_printf("010.5u : %010.5ubonjour\n", 12));
+	printf("return : %d\n",ft_printf("-10.5u : %-10.5ubonjour\n", 12));
+	printf("return : %d\n",ft_printf("0*u : %0*ubonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("-*u : %-*ubonjour\n", 10, 12));
+	printf("return : %d\n",ft_printf("FOR s  :: ---------------------------\n"));
+	printf("return : %d\n",ft_printf("simple test : %s \n", "bonjour"));
+	printf("return : %d\n",ft_printf(".5s : %.5s \n", "bonjour"));
+	printf("return : %d\n",ft_printf(".*s : %.*s \n", 5, "bonjour"));
+	printf("return : %d\n",ft_printf("-10.5s : %-5.5s \n", "bonjour"));
+	printf("return : %d\n",ft_printf("-*.*s : %-10.5s \n", "bonjour"));
+	printf("return : %d\n",ft_printf("-*s : %-*s \n", 5, "bonjour"));
+	printf("return : %d\n",ft_printf("5s : %12s \n", "bonjour"));
+	printf("return : %d\n",ft_printf("*s : %*s \n", 12, "bonjour"));
+	printf("return : %d\n",ft_printf("double arg : %s%s\n", "bonjour", "paul"));
+	ft_printf("-*.*s : %-*.*s \n", 90, 5, "bonjour");
 }
-	*/
+
 int main(int ac, char **av)
 {
 	if (ac - 1 > 0)
 	{
 		if(av[1][0] == 'd')
+		{
+			test_all_flag();
 			d();
+		}
 	}
 	else
 		ft_putstr("You can use ./a.out d for instance");
--- srcs/pf_s.c
+++ srcs/pf_s.c
@@ -6,39 +6,72 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/23 14:02:15 by thbeaumo          #+#    #+#             */
-/*   Updated: 2020/01/19 15:07:37 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2020/01/27 11:02:30 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Libft/libft.h"
 #include "../headers/ft_printf.h"
 
-t_struct pf_s(va_list ap, t_struct datas, int i, const char *s)
+static t_struct	ft_display_val(t_struct datas, char *val, int prec_len)
 {
-    char *val;
-    int j;
-	int prec_len;
-    (void)s;
+	int j;
 
-    j = 0;
-	datas.flag[temp] = i;
-    if((val = va_arg(ap, char *)) == NULL)
-    {
-        val = "(null)";
-        datas = ft_buffer('\0', datas);
-        return (datas);
-    }
-	prec_len = (datas.flag[size_prec] > 0 ?
-		   	datas.flag[size_prec] : (int)ft_strlen(val));
-	if ((!datas.flag[flags] && (!datas.flag[prec] || datas.flag[size_prec] == 0)
-	 && datas.flag[size] > 0))
-		datas = fill_size(datas, (int)ft_strlen(val));
-    while (val[j])
+	j = 0;
+	while (val[j])
 	{
-		while (j < prec_len)
-			datas = ft_buffer(val[j++], datas);
+		if (j < prec_len)
+			datas = ft_buffer(val[j], datas);
 		if (j >= prec_len)
 			break ;
+		j++;
 	}
-    return (datas);
+	return (datas);
+}
+
+t_struct	fill_size_s(t_struct datas, int len_arg)
+{
+	int prec_len;
+//si il y a une prec prendre la diff entre size et size_prec et mettre des spaces while > 0 
+	prec_len = datas.flag[size_prec] > len_arg &&
+	   	datas.flag[size] < datas.flag[size_prec] ?
+	   	datas.flag[size_prec] - len_arg : 0;
+	while (datas.flag[size] - len_arg > 0 && 
+			datas.flag[size] - len_arg > prec_len) 
+	{
+		datas = ft_buffer(' ', datas);
+		datas.flag[size]--;
+	}
+	return (datas);
+}
+
+t_struct pf_s(va_list ap, t_struct datas, int i, const char *s)
+{
+	char *val;
+	int j;
+	int len;
+	int prec_len;
+	(void)s;
+
+	j = 0;
+	val = va_arg(ap, char *);
+	datas.flag[temp] = i;
+	len = (int)ft_strlen(val);
+	prec_len = (datas.flag[size_prec] > 0 ?
+			datas.flag[size_prec] : len);
+	prec_len = (datas.flag[prec] == '.' && datas.flag[size_prec] == 0 &&
+			datas.flag[size] == 0) ? 0 : prec_len;
+	if ((!datas.flag[flags] && datas.flag[size] > 0) ||
+			(!datas.flag[flags] && datas.flag[size_prec] > 0))
+		datas = fill_size_s(datas, len);
+	if(val == NULL)
+	{
+		val = "(null)";
+		datas = ft_display_val(datas, val, prec_len);
+		return (datas);
+	}
+	datas = ft_display_val(datas, val, prec_len);
+	if (datas.flag[flags] == '-')
+		datas = ft_right_justify(datas, len, 0);
+	return (datas);
 }
