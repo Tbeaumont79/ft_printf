9d26739fabcdd82d69abd4076f079fb6f569ab3e
thbeaumo <thbeaumo@student.42.fr>
2020-02-07 18:29:10 +0100

    adding a major fix now good for 2 checker need to fix the parsing

--- srcs/ft_neg_itoa.c
+++ srcs/ft_neg_itoa.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/20 10:07:02 by thbeaumo          #+#    #+#             */
-/*   Updated: 2020/02/04 11:45:47 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2020/02/07 18:17:57 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,29 +17,29 @@ t_struct	fill_size_neg(t_struct datas, int len_arg)
 {
 	int prec_len;
 
-	prec_len = (datas.flag[size_prec] > len_arg ?
-			datas.flag[size_prec] - len_arg : 0);
-	while (datas.flag[size] - len_arg - 1 > prec_len)
-	{
+	datas.flag[size_prec] += datas.flag[neg] != 2;	
+	prec_len = datas.flag[size_prec] > len_arg ? datas.flag[size_prec] : len_arg;
+	while ((datas.flag[size]--) > prec_len)
 		datas = ft_buffer(' ', datas);
-		datas.flag[size]--;
-	}
-	if (prec_len > 0)
+	if (datas.flag[prec] == '.')
 	{
-		datas = datas.flag[neg] == 0 ? ft_buffer('-', datas) : datas;
-		datas.flag[neg] = 1;
+		if (prec_len > 0)
+		{
+			datas = datas.flag[neg] == 0 ? ft_buffer('-', datas) : datas;
+			datas.flag[neg] = 1;
+		}
+		while (prec_len > len_arg)
+		{
+			datas = ft_buffer('0', datas);
+			prec_len--;
+		}
 	}
-	while (prec_len > 0)
-	{
-		datas = ft_buffer('0', datas);
-		prec_len--;
-	}
+
 	return (datas);
 }
 
 t_struct	fill_if_neg(t_struct datas, int sizes)
 {
-	sizes--;
 	if ((!datas.flag[flags] && datas.flag[size] > 0) ||
             (!datas.flag[flags] && datas.flag[size_prec] > 0))
         datas = fill_size_neg(datas, sizes);
@@ -47,7 +47,7 @@ t_struct	fill_if_neg(t_struct datas, int sizes)
     {
         if (datas.flag[neg] == 0)
             datas.flag[neg] = 1;
-		sizes += datas.flag[neg] == 2;
+		sizes -= datas.flag[neg] != 2;	
         datas = ft_left_justify(datas, sizes);
     }
     return (datas);
@@ -63,14 +63,14 @@ t_struct	display_neg_str(t_struct datas, char *neg_str, long long nb, int base)
 
     nb = base == 10 ?  ft_abs(nb) : nb;
     sizes = nb_len(nb, base);
-    l = datas.flag[size_prec] > sizes ? datas.flag[size_prec] - sizes - 1 : 0; 
     tab = "0123456789ABCDEF";
     neg_str[sizes] = '\0';
     i = 0;
-    tmp = sizes;
+    tmp = 0;
     while (sizes > 0)
     {
         sizes--;
+		tmp++;
         neg_str[sizes] = tab[nb % base];
         nb = nb / base;
     }
@@ -79,12 +79,12 @@ t_struct	display_neg_str(t_struct datas, char *neg_str, long long nb, int base)
 		datas = ft_buffer('-', datas);
 		datas.flag[neg] = 1;
 	}
+	sizes = datas.flag[size_prec];
+	l = datas.flag[size_prec] > tmp ? datas.flag[size_prec] - tmp : tmp;
+	if ((datas.flag[conv] == 'x' || datas.flag[conv] == 'X'))
+		datas = fill_if_neg(datas, tmp);
     if (datas.flag[flags] == '-' && datas.flag[prec] == '.')
-	{
-		tmp += datas.flag[size] > datas.flag[size_prec] &&
-		   	datas.flag[size_prec] - tmp - 1 ? 1 : 0;
-        datas = fill_right_justify_prec(datas, tmp, l);
-	}
+		datas = fill_right_justify_prec(datas, tmp, l);
 	if (datas.flag[conv] == 'x')
 		neg_str = string_lower(neg_str); 
     while (neg_str[i])
@@ -93,7 +93,13 @@ t_struct	display_neg_str(t_struct datas, char *neg_str, long long nb, int base)
         i++;
     }
     if (datas.flag[flags] == '-')
-        datas = ft_right_justify(datas, tmp, l);
+	{
+		tmp = (datas.flag[conv] == 'd' || datas.flag[conv] == 'i') ? tmp + 1: tmp;
+		tmp = sizes > tmp && datas.flag[prec] == '.' && (datas.flag[conv] == 'x' || datas.flag[conv] == 'X') ? tmp + (sizes - tmp) : tmp;
+		tmp = sizes > tmp && datas.flag[prec] == '.' && (datas.flag[conv] == 'i' || datas.flag[conv] == 'd') ? tmp + (sizes - tmp + 1) : tmp;
+		while (datas.flag[size]-- > tmp)
+			datas = ft_buffer(' ', datas);
+	}
     free(neg_str);
     return (datas);
 }
@@ -112,18 +118,17 @@ t_struct	handle_neg(t_struct datas, long long nb, int base, int prec_len)
     {
         if (!(neg_str = (char *)malloc(sizeof(char) * (sizes + 1))))
             return (datas);
-        datas = fill_if_neg(datas, sizes);
+		if ((datas.flag[conv] == 'd' || datas.flag[conv] == 'i'))
+			datas = fill_if_neg(datas, sizes);
         return (display_neg_str(datas, neg_str, nb, 10));
     }
 	if (nb < 0 && base == 16)
 	{
-		datas.flag[neg] = 2;
         nb = 4294967295 + nb + 1;
+		datas.flag[neg] = 2;
 		sizes = nb_len(nb, base);
-        datas = fill_if_neg(datas, sizes);
 		if (!(neg_str = (char *)malloc(sizeof(char) * (sizes + 1))))
 			return (datas);
-		prec_len = datas.flag[size_prec] > sizes ? datas.flag[size_prec] - sizes : 0; 
         return (display_neg_str(datas, neg_str, nb, 16));
 	}
     return (datas);
--- srcs/pf_d.c
+++ srcs/pf_d.c
@@ -6,7 +6,7 @@
 /*   By: thbeaumo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/02 17:19:01 by thbeaumo          #+#    #+#             */
-/*   Updated: 2020/02/06 16:31:55 by thbeaumo         ###   ########.fr       */
+/*   Updated: 2020/02/07 14:59:43 by thbeaumo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,75 +15,72 @@
 
 t_struct fill_right_justify_prec(t_struct datas, int len_arg, int prec_len)
 {
-    prec_len = datas.flag[size_prec] > len_arg ? datas.flag[size_prec] : len_arg;
-    prec_len += datas.flag[neg] == 1 && prec_len > len_arg + 1;
-    prec_len -= datas.flag[conv] == 'x' || datas.flag[conv] == 'X';
-    if (datas.flag[prec] == '.' && prec_len > len_arg)
-    {
-        while (prec_len > len_arg)
-        {
-            datas = ft_buffer('0', datas);
-            prec_len--;
-        }
-    }
-    
-    return (datas);
+	prec_len = datas.flag[size_prec] > len_arg ? datas.flag[size_prec] : len_arg;
+	if (datas.flag[prec] == '.' && prec_len > len_arg)
+	{
+		while (prec_len > len_arg)
+		{
+			datas = ft_buffer('0', datas);
+			prec_len--;
+		}
+	}
+
+	return (datas);
 }
 
 t_struct	fill_size(t_struct datas, int len_arg)
 {
-    int prec_len;
+	int prec_len;
 
-    prec_len = datas.flag[size_prec] > len_arg ? datas.flag[size_prec] : len_arg;
-    prec_len -= datas.flag[neg] == 1;
-    while ((datas.flag[size]--) > prec_len)
-        datas = ft_buffer(' ', datas);
-    if (datas.flag[prec] == '.')
-    {
-        while (prec_len > len_arg)
-        {
-            datas = ft_buffer('0', datas);
-            prec_len--;
-        }
-    }
-    return (datas);
+	prec_len = datas.flag[size_prec] > len_arg ? datas.flag[size_prec] : len_arg;
+	while ((datas.flag[size]--) > prec_len)
+		datas = ft_buffer(' ', datas);
+	if (datas.flag[prec] == '.')
+	{
+		while (prec_len > len_arg)
+		{
+			datas = ft_buffer('0', datas);
+			prec_len--;
+		}
+	}
+	return (datas);
 }
 
 t_struct pf_d(va_list ap, t_struct datas, int i, const char *s)
 {
-    int val;
-    int j;
-    char *stringValue;
-    int len;
-    int prec_len;
+	int val;
+	int j;
+	char *stringValue;
+	int len;
+	int prec_len;
 
-    (void)s;
-    j = 0;
-    datas.flag[temp] = i;
-    val = va_arg(ap, int);
-    stringValue = ft_itoa_base(datas, val, 10);
-    len = ft_strlen(stringValue);
-    len = ((val == 0 && (datas.flag[prec] == '.' &&
-                    datas.flag[size_prec] == 0)) ? 0 : ft_strlen(stringValue));
-    prec_len = 0;
-    if (val >= 0)
-    {
-        if ((!datas.flag[flags] && datas.flag[size] > 0) ||
-                (!datas.flag[flags] && datas.flag[size_prec] > 0))
-            datas = fill_size(datas, len);
-        if (datas.flag[flags] == '0')
-            datas = ft_left_justify(datas, (int)ft_strlen(stringValue));
-        if (datas.flag[prec] == '.' && datas.flag[flags] != '0' && datas.flag[flags])
-            datas = fill_right_justify_prec(datas, (int)ft_strlen(stringValue), len);
-        while (stringValue[j] && !(val == 0 && datas.flag[prec] == '.' && datas.flag[size_prec] == 0))
-            datas = ft_buffer(stringValue[j++], datas);
-        len = len > datas.flag[size_prec] ? len : datas.flag[size_prec];
-        if (datas.flag[flags] == '-')
-            while (datas.flag[size]-- > len)
-                datas = ft_buffer(' ', datas);
-        free(stringValue);
-    }
-    else
-        datas = handle_neg(datas, val, 10, len);
-    return (datas);
+	(void)s;
+	j = 0;
+	datas.flag[temp] = i;
+	val = va_arg(ap, int);
+	stringValue = ft_itoa_base(datas, val, 10);
+	len = ft_strlen(stringValue);
+	len = ((val == 0 && (datas.flag[prec] == '.' &&
+					datas.flag[size_prec] == 0)) ? 0 : ft_strlen(stringValue));
+	prec_len = 0;
+	if (val >= 0)
+	{
+		if ((!datas.flag[flags] && datas.flag[size] > 0) ||
+				(!datas.flag[flags] && datas.flag[size_prec] > 0))
+			datas = fill_size(datas, len);
+		if (datas.flag[flags] == '0')
+			datas = ft_left_justify(datas, (int)ft_strlen(stringValue));
+		if (datas.flag[prec] == '.' && datas.flag[flags] != '0' && datas.flag[flags])
+			datas = fill_right_justify_prec(datas, (int)ft_strlen(stringValue), len);
+		while (stringValue[j] && !(val == 0 && datas.flag[prec] == '.' && datas.flag[size_prec] == 0))
+			datas = ft_buffer(stringValue[j++], datas);
+		len = len > datas.flag[size_prec] ? len : datas.flag[size_prec];
+		if (datas.flag[flags] == '-')
+			while (datas.flag[size]-- > len)
+				datas = ft_buffer(' ', datas);
+		free(stringValue);
+	}
+	else
+		datas = handle_neg(datas, val, 10, len);
+	return (datas);
 }
